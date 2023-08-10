ffbfd98cc6267f0097417c46bbd50cd48ae870b8
Rasmad Ibnu <rasmad.ibnu@smartfinance.co.id>
2023-08-10 09:40:32 +0700

    change color table

--- dist/spa/assets/QCardActions.8d8bc00a.js
+++
@@ -1,1 +0,0 @@
-import{c as t,ag as r,ah as c,a as n,h as l,b as i}from"./index.5cd558a4.js";var d=t({name:"QCardActions",props:{...r,vertical:Boolean},setup(a,{slots:s}){const e=c(a),o=n(()=>`q-card__actions ${e.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>l("div",{class:o.value},i(s.default))}});export{d as Q};
